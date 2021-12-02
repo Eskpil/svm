11446fdea9d569f994c0ed94249dc055f648f92d
linus <linusen.a@gmail.com>
2021-12-02 22:26:29 +0100

    cleaned up run script

--- run.sh
+++ run.sh
@@ -2,5 +2,15 @@
 
 set -x
 
-gcc -o build/$1 $1.c
-build/$1
+if [ -z $1 ]; then
+    echo "You need to specify project, example ./run.sh svm" 
+else 
+    if [ -d "build" ]; then
+        gcc -o build/$1 $1.c
+        build/$1
+    else 
+        mkdir build
+        gcc -o build/$1 $1.c
+        build/$1
+    fi
+fi
