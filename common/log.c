e416ec8e50aeeacaeabe5310b4be70b77e175f49
linus <linusen.a@gmail.com>
2022-01-01 23:25:34 +0100

    revamped run script, making it a bit more modular and expressive

---
+++ common/log.c
@@ -0,0 +1,14 @@
+#include "./log.h"
+
+void note(const char *message) {
+    assert(0 && "Note logging is not implemented");
+}
+
+void fatal(const char *message) {
+    assert(0 && "Fatal logging is not implemented");
+}
+
+void warning(const char *message) {
+    assert(0 && "Warning logging is not implemented");
+}
+
